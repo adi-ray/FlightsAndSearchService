7d3aa270b957c7ee0201aa68a90e7994f4f454de
adi-ray <adityadns03@gmail.com>
2025-04-16 01:44:06 +0530

    feat: Add CityController for city management operations

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,90 @@
+const { CityService } = require("../services/index");
+
+const cityService = new CityService();
+
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      messsage: "City created successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      messsage: "City creation failed",
+      err: error,
+    });
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const response = await cityService.deleteCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      messsage: "City deleted successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      messsage: "City deletion failed",
+      err: error,
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const response = await cityService.updateCity(req.params.id, req.body);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      messsage: "City updated successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      messsage: "City update failed",
+      err: error,
+    });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const response = await cityService.getCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      messsage: "City fetched successfully",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      messsage: "City fetch failed",
+      err: error,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  destroy,
+  update,
+  get,
+};
