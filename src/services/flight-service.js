1ab3501f067fbf3a4174a9afd5ef66a391222e4f
adi-ray <adityadns03@gmail.com>
2025-04-24 03:12:46 +0530

    feat: Enhance flight creation with time comparison utility

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,4 +1,5 @@
 const { FlightRepository, AirplaneRepository } = require("../repository/index");
+const { compareTime } = require("../utils/helper");
 
 class FlightService {
   constructor() {
@@ -8,6 +9,9 @@ class FlightService {
 
   async createFlight(data) {
     try {
+      if (!compareTime(data.arrivalTime, data.departureTime)) {
+        throw { error: "Arrival time cannot be less than departure time" };
+      }
       const airplane = await airplaneRepository.getAirplane(data.airplaneId);
       const flight = await flightRepository.createFlight({
         ...data,
