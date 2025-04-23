e247ad8d4f99a7f1830d2708e63131480f1f5058
adi-ray <adityadns03@gmail.com>
2025-04-24 02:43:06 +0530

    feat: Implement common CRUD classes for basic CRUD operations

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,6 @@
 module.exports = {
   CityService: require("./city-service"),
   FlightService: require("./flight-service"),
+  AiportService: require("./airport-service"),
+  CrudService: require("./crud-service"),
 };
